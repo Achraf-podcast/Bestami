3b0515fd4c7334ee397cce2c95b4d87c0f35fd9c
Achraf-podcast <achrafagourram6@gmail.com>
2025-10-29 12:43:55 +0100

    Adding comments and make the code more clear

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,22 @@
+// Afficher le formulaire
 function displayTransactionFormulaire(){
     let formulaire = document.getElementsByClassName("formulaire");
     formulaire[0].style.display = "flex";
 }
+// Fermer le formulaire ou annuler la transaction
 function CloseFormulaire(){
     let formulaire = document.getElementsByClassName("formulaire");
     formulaire[0].style.display = "none";
 }
 
+// Declare buttons
 const newTransBtn = document.getElementById("addNewTransaction");
 const closeFormulaireBtns = document.getElementsByClassName("close-formulaire")
+const incomeBtn = document.getElementById("income-btn")
+const expenseBtn = document.getElementById("expense-btn")
 
-
+// make buttons functionel
 newTransBtn.addEventListener("click", displayTransactionFormulaire);
-
 for(let i=0; i<closeFormulaireBtns.length; i++){
     closeFormulaireBtns[i].addEventListener("click", CloseFormulaire)
 }
