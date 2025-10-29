43e67f54eaed264dbc5ec9d93db59f890673445f
Achraf-podcast <achrafagourram6@gmail.com>
2025-10-29 12:24:00 +0100

    Make the add transaction and close buttons functionel with js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+function displayTransactionFormulaire(){
+    let formulaire = document.getElementsByClassName("formulaire");
+    formulaire[0].style.display = "flex";
+}
+function CloseFormulaire(){
+    let formulaire = document.getElementsByClassName("formulaire");
+    formulaire[0].style.display = "none";
+}
+
+const newTransBtn = document.getElementById("addNewTransaction");
+const closeFormulaireBtns = document.getElementsByClassName("close-formulaire")
+
+
+newTransBtn.addEventListener("click", displayTransactionFormulaire);
+
+for(let i=0; i<closeFormulaireBtns.length; i++){
+    closeFormulaireBtns[i].addEventListener("click", CloseFormulaire)
+}
