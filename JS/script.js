de26f37120ae2bb7716cc023d42f3a8c0df0680b
Achraf-podcast <achrafagourram6@gmail.com>
2025-10-29 14:37:15 +0100

    make the Income and expense buttons responsive

--- JS/script.js
+++ JS/script.js
@@ -8,6 +8,22 @@ function CloseFormulaire(){
     let formulaire = document.getElementsByClassName("formulaire");
     formulaire[0].style.display = "none";
 }
+function income(){
+    expenseBtn.classList.remove("bg-white");
+    expenseBtn.classList.remove("text-green-600");
+    expenseBtn.classList.remove("transition-all");
+    incomeBtn.classList.add("bg-white");
+    incomeBtn.classList.add("text-green-600");
+    incomeBtn.classList.add("transition-all");
+}
+function expense(){
+    incomeBtn.classList.remove("bg-white");
+    incomeBtn.classList.remove("text-green-600");
+    incomeBtn.classList.remove("transition-all");
+    expenseBtn.classList.add("bg-white");
+    expenseBtn.classList.add("text-red-600");
+    expenseBtn.classList.add("transition-all");
+}
 
 // Declare buttons
 const newTransBtn = document.getElementById("addNewTransaction");
@@ -18,5 +34,7 @@ const expenseBtn = document.getElementById("expense-btn")
 // make buttons functionel
 newTransBtn.addEventListener("click", displayTransactionFormulaire);
 for(let i=0; i<closeFormulaireBtns.length; i++){
-    closeFormulaireBtns[i].addEventListener("click", CloseFormulaire)
+    closeFormulaireBtns[i].addEventListener("click", CloseFormulaire);
 }
+incomeBtn.addEventListener("click", income);
+expenseBtn.addEventListener("click", expense);
